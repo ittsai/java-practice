cf3b72f38bb21082d14497654ffbcd3d6ac50176
I-Ting <it.tsai.1113@gmail.com>
2025-02-18 19:59:05 +0100

    Add GraphQL way

--- monday.platform/src/main/java/com/integration/monday/platform/service/MondayWebClientService.java
+++ monday.platform/src/main/java/com/integration/monday/platform/service/MondayWebClientService.java
@@ -1,6 +1,5 @@
 package com.integration.monday.platform.service;
 
-import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -9,13 +8,23 @@ public class MondayWebClientService {
 
     private final WebClient webClient;
 
+    private final String MONDAY_API_URL = "https://api.monday.com/v2";
+    // https://developer.monday.com/api-reference/docs/authentication
+    private final String API_TOKEN = "your-api-token";
+
+
     public MondayWebClientService(WebClient.Builder webClientBuilder) {
         this.webClient = webClientBuilder.baseUrl("").build();
     }
 
     public String fetchData() {
-        return this.webClient.get()
-                .uri("/")
+        String graphqlQuery = "{ \"query\": \"query { boards { id name items { id name } } }\" }";
+
+        return this.webClient.post()
+                .uri(MONDAY_API_URL)
+                .header("Authorization", "Bearer "+API_TOKEN)
+                .header("Content-Type", "application/json")
+                .bodyValue(graphqlQuery)
                 .retrieve()
                 .bodyToMono(String.class)
                 .block(); // for sync
